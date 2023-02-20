8582da25354a3ab2565e448e3bda3034a67b5355
irmeliagst <ameliaagasta2002@gmail.com>
2023-02-20 08:19:43 +0700

    first commit

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,7 +14,7 @@
 
   <nav class="navbar navbar-expand-lg navbar-dark bg-blue">
   <div class="container">
-    <a class="navbar-brand">EKA BLOG</a>
+    <a class="navbar-brand">AMELIA BLOG</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,50 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/about', function(){
+    return view('about',[
+        "judul" => "About",
+        "nama" => "IRA AMELIA",
+        "Nim" => "E41210501",
+        "image" => "yoona.jpg"
+    ]);
+});
+Route::get('/dashboard', function(){
+    return view('dashboard',[
+        "judul" => "Dashboard"
+    ]);
+});
+Route::get('/blog', function(){
+    $blog_post=[
+        [
+            "judul" => "Blog Pertama",
+            "Penulis" => "IRA AMELIA",
+            "body" =>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+            A reprehenderit dolore eaque beatae pariatur exercitationem quisquam in magni quia ipsam, sit dolores. 
+            Consectetur veniam dolore officiis obcaecati quidem distinctio perspiciatis eveniet, 
+            porro molestiae magnam, atque asperiores harum minima? Rem quam porro nam soluta ullam, 
+            eligendi dolor ipsum. Vero quos ipsam, mollitia illo, sint dolores neque, accusantium adipisci ex 
+            nobis quisquam laudantium corporis qui repudiandae. 
+            Quis, inventore dignissimos. Recusandae enim magnam architecto id temporibus commodi sed nihil velit.
+            Mollitia deserunt veritatis reprehenderit sunt quod laborum quam. 
+            Quia veniam expedita doloribus, quisquam animi inventore, quis rerum dolorem quibusdam quidem cum fugiat fuga."
+        ],
+        [
+            "judul" => "Blog Kedua",
+            "Penulis" => "AMALIA",
+            "body" =>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+            A reprehenderit dolore eaque beatae pariatur exercitationem quisquam in magni quia ipsam, sit dolores. 
+            Consectetur veniam dolore officiis obcaecati quidem distinctio perspiciatis eveniet, 
+            porro molestiae magnam, atque asperiores harum minima? Rem quam porro nam soluta ullam, 
+            eligendi dolor ipsum. Vero quos ipsam, mollitia illo, sint dolores neque, accusantium adipisci ex 
+            nobis quisquam laudantium corporis qui repudiandae. 
+            Quis, inventore dignissimos. Recusandae enim magnam architecto id temporibus commodi sed nihil velit.
+            Mollitia deserunt veritatis reprehenderit sunt quod laborum quam. 
+            Quia veniam expedita doloribus, quisquam animi inventore, quis rerum dolorem quibusdam quidem cum fugiat fuga."
+        ]
+        ];
+    return view('blog',[
+        "judul" => "Blog",
+        "postingan"=> $blog_post
+    ]);
+});
